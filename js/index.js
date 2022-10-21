b2a9e536d2ce04f8a5a58631bee1a2d39c4aeb96
Caleb McCoy <caleb_mccoy115@yahoo.com>
2022-10-21 10:38:14 -0500

    add button to our URL, updated where that url is located, at bottom, styled li element

--- js/index.js
+++ js/index.js
@@ -14,7 +14,10 @@ gitForm.addEventListener('submit', (e) => {
   })
     .then((response) => response.json())
     .then((data) => {
-      renderUser(data.items[0])
+      //renderUser(data.items[0])
+        data.items.forEach(user =>{
+          renderUser(user)
+        })
     })
     })
 
@@ -27,17 +30,31 @@ gitForm.addEventListener('submit', (e) => {
 
 function renderUser(user) {
   const li = document.createElement('li')
+  li.style.display = 'flex';
+  li.style.flexDirection = 'column';
+  
+
+
+
   const username = document.createElement('h1')
   const avatar = document.createElement('img')
-  const pageLink = document.createElement('a')
+  //const pageLink = document.createElement('a')
 
   username.textContent = user.login
   avatar.src = user.avatar_url
-  pageLink.href = user.html_url
-  pageLink.textContent = `Go to ${user.login}'s page!`
-
+  //pageLink.href = user.html_url
+  
+  // username.classList = "userdatacontainer"
+  // avatar.classList = "userdatacontainer"
+  const urlButton = document.createElement("button")
+  urlButton.textContent = `Go to ${user.login}'s page!`
+  urlButton.addEventListener("click", () => {
+    window.location.assign(user.html_url)
+  })
+  
+  
   userList.append(li)
-  li.append(username, avatar, pageLink)
+  li.append(username, avatar, urlButton)
   console.log()
+  
 }
-
